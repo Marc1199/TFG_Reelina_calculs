519068579c99bb121eae84a48cd27c416f33d644
Marc1199 <marc.tomas@uvic.cat>
2025-04-03 15:50:24 +0200

    Fix formatting in minim.sh by adding a trailing newline

--- minim.sh
+++ minim.sh
@@ -5,5 +5,5 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1        # Nombre de CPUs per tasca
 #SBATCH --mem=2GB                 # Memòria assignada
-#SBATCH --partition=highmem     # Tipus de partició
+#SBATCH --partition=highmem     # Tipus de partició  
 $AMBERHOME/bin/pmemd -O -i 01_Min.in -o 01_Min.out -p parm7 -c rst7 -r 01_Min.ncrst -inf 01_Min.mdinfo
