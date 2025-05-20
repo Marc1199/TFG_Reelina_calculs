80c6cbda84ddd7ad2ab87b37fec1d0f62ef6750e
Marc Tomas <marctommon@gmail.com>
2025-05-20 17:06:31 +0200

    sequencies eliminades

--- Clauster/fer_simulacio.sh
+++ Clauster/fer_simulacio.sh
@@ -1,18 +1,10 @@
-#!/usr/bin/bash
-#SBATCH --job-name=Tutorial_amber
-#SBATCH --output=sortida.txt
-#SBATCH --error=error.txt
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=2GB
-#SBATCH --partition=highmem
-
 # Directorio donde están los archivos
-DIRECTORIO=~/TFG_reelina_calculs/Clauster/conformacions
+DIRECTORIO=/mnt/d/Marc/TFG/conformacions
 # Directorio donde se guardarán las salidas
 SALIDAS_DIR=~/sortides_MD_reelina
 
 # Crear el directorio de salidas si no existe
+
 mkdir -p $SALIDAS_DIR
 
 # Iterar sobre todos los archivos .parm7 que empiecen por "sequencia_"
@@ -25,6 +17,16 @@ for parm_file in $DIRECTORIO/sequencia_*.parm7; do
     if [[ -f "$rst_file" ]]; then
         echo "Procesando $base_name..."
 
+        cat << EOF > $SALIDAS_DIR/submit$base_name.sh
+#!/usr/bin/bash
+#SBATCH --job-name=Tutorial_amber
+#SBATCH --output=sortida.txt
+#SBATCH --error=error.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=2GB
+#SBATCH --partition=highmem
+
         # Crear un subdirectorio para cada archivo
         mkdir -p "$SALIDAS_DIR/$base_name"
 
@@ -32,6 +34,13 @@ for parm_file in $DIRECTORIO/sequencia_*.parm7; do
         $AMBERHOME/bin/pmemd -O -i Min.in -o "$SALIDAS_DIR/$base_name/Min.out" -p "$parm_file" -c "$rst_file" -r "$SALIDAS_DIR/$base_name/Min.ncrst" -inf "$SALIDAS_DIR/$base_name/Min.mdinfo"
         $AMBERHOME/bin/pmemd -O -i Heat.in -o "$SALIDAS_DIR/$base_name/Heat.out" -p "$parm_file" -c "$SALIDAS_DIR/$base_name/Min.ncrst" -r "$SALIDAS_DIR/$base_name/Heat.ncrst" -x "$SALIDAS_DIR/$base_name/Heat.nc" -inf "$SALIDAS_DIR/$base_name/Heat.mdinfo"
         $AMBERHOME/bin/pmemd -O -i Prod.in -o "$SALIDAS_DIR/$base_name/Prod.out" -p "$parm_file" -c "$SALIDAS_DIR/$base_name/Heat.ncrst" -r "$SALIDAS_DIR/$base_name/Prod.ncrst" -x "$SALIDAS_DIR/$base_name/Prod.nc" -inf "$SALIDAS_DIR/$base_name/Prod.mdinfo"
+EOF
+    sbatch $SALIDAS_DIR/submit$base_name.sh
+
+        # Limpiar el script después de enviarlo
+        #rm submit$base_name.sh
+
+        echo "✅ Simulación para $base_name iniciada."
     else
         echo "⚠️ Archivo .rst7 no encontrado para $base_name, se omite."
     fi
